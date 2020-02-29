1cd8f257598607a3c4c795f49e706a15a3f8d8a5
Charles Kerr <ckerr@github.com>
2020-02-29 13:00:20 -0600

    feat: add canvas placeholder
    Taken from the 'A simple example' code in the 2D canvas tutorial at
    https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 
+function draw() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+document.addEventListener("DOMContentLoaded", draw);
+
 
